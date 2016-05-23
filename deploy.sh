70c5c10ebab9afefdb99406824ab60a418fa4e95
Shaoshuai Dong <dongshaoshuai@ucloud.cn>
2016-05-23 15:56:18 +0800

    Update version to 0.1.1

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ $# != 1 ]; then
+    echo "Please enter commit message"
+    echo "Example:"
+    echo "./deploy.sh \"update README\""
+
+    exit 1
+fi
+
+COMMIT_MSG="$1"
+
+echo "================================="
+echo "Begin to deploy"
+echo "================================="
+
+git add . && git commit -m "${COMMIT_MSG}" && git push origin master
+
+echo "================================="
+echo "Deployed: master"
+echo "================================="
+
+git checkout gh-pages && git add . && git commit -m "${COMMIT_MSG}" && git push origin gh-pages
+
+echo "================================="
+echo "Deployed: gh-pages"
+echo "================================="
+
+git checkout master
